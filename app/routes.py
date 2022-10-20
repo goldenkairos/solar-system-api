1ae2555cb368664160c35a719f5a9c847c34df78
Minh <minh.seikel@gmail.com>
2022-10-20 14:11:10 -0700

    completing /planets

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,5 @@
 from flask import Flask
 
-def create_app():
-    app = Flask(__name__)
-    
-    from .routes.planets import planets_bp
-    app.register_blueprint(planets_bp)
-    return app
+
+
 
